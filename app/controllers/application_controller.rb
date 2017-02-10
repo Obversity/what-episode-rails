62e95c99787a23ea6b9f9f4ef559497dfbc61589
bill <bill.j.pearce@gmail.com>
2017-02-11 00:15:30 +1030

    updated authentication to use header-based token rather than params based token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,9 @@ class ApplicationController < ActionController::API
   private
 
   def authorize!
-    token = params[:token]
-    @user = User.find_by(token: token)
+    # get <token> from { 'Authorization' => 'Bearer <token>'}
+    token = request.headers['Authorization'].try(:split, ' ').try(:second)
+    @user = token.present? ? User.find_by(token: token) : nil
     if @user.blank? || token.blank?
       render json: { error: 'You are not authorized to do this' }, status: 401
     end
