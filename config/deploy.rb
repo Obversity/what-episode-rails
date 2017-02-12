f8e4c636d171214843881663cd0771858917dd1d
bill <bill.j.pearce@gmail.com>
2017-02-12 22:07:38 +1030

    fixed issues with puma capistrano config

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,8 +10,8 @@ set :puma_workers,    0
 set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
-set :puma_access_log, "#{release_path}/log/puma.error.log"
-set :puma_error_log,  "#{release_path}/log/puma.access.log"
+set :puma_access_log, "#{release_path}/log/puma.access.log"
+set :puma_error_log,  "#{release_path}/log/puma.error.log"
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
@@ -46,7 +46,6 @@ namespace :deploy do
     end
   end
 
-  before :starting,     :check_revision
   after  :finishing,    :cleanup
   after  :finishing,    :restart
 end
