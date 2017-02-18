e55663fb8bcaffa9c76d39caa24b76f080e65dfb
bill <bill.j.pearce@gmail.com>
2017-02-18 13:25:26 +1030

    fixed dsl error in schedule rb

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,6 +3,6 @@ cap_path = "/var/www/what_episode_rails"
 
 set :output, "#{cap_path}/shared/log/cron_log.log"
 
-ever 1.week do
+every 1.week do
   comand "ruby #{cap_path}/current/lib/get_shows.rb"
 end
