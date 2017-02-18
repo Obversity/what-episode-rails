178b86930f8ca446d89450ca58c03cad26fddcc5
bill <bill.j.pearce@gmail.com>
2017-02-18 15:36:48 +1030

    changed scheduled job starter to use bundle exec ruby, so the right gems are available

--- config/schedule.rb
+++ config/schedule.rb
@@ -4,5 +4,5 @@ cap_path = "/var/www/what_episode_rails"
 set :output, "#{cap_path}/shared/log/cron_log.log"
 
 every 1.week do
-  command "ruby #{cap_path}/current/lib/tasks/get_shows.rb"
+  command "bundle exec ruby /var/www/what_episode_rails/current/lib/tasks/get_shows.rb"
 end
