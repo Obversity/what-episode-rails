988ac19ba2da2452c4130a16a26babdb3272a43f
bill <bill.j.pearce@gmail.com>
2017-03-10 20:12:27 +1030

    updated various things in preparation for deploy to new vultr server

--- config/deploy.rb
+++ config/deploy.rb
@@ -125,8 +125,7 @@ end
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+set :deploy_to, "/data/what_episode_rails"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
--- config/schedule.rb
+++ config/schedule.rb
@@ -1,8 +1,8 @@
 
-cap_path = "/var/www/what_episode_rails"
+cap_path = "/data/what_episode_rails"
 
 set :output, "#{cap_path}/shared/log/cron_log.log"
 
 every 1.week do
-  command "bundle exec ruby /var/www/what_episode_rails/current/lib/tasks/get_shows.rb"
+  command "bundle exec ruby #{cap_path}/current/lib/tasks/get_shows.rb"
 end
